79f41280457ed035b6b8e28f60cd5eeb754b406e
Carlos Zamora Morales <zamorx@gmail.com>
2023-02-17 16:52:42 -0600

    Update notify.php

--- dashboard/View/notify.php
+++ dashboard/View/notify.php
@@ -17,7 +17,7 @@ try {
     $mail->Host = 'smtp.office365.com';                   // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
     $mail->Username = 'noreply@azedpress.com';            // SMTP username
-    $mail->Password = 'domnaz-nukqe1-joMzom';                    // SMTP password
+    $mail->Password = 'domnaz-nukqe1-joMzom';             // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
