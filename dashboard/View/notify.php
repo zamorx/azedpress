d5547290dd37c38216e0ee49b09a4c83ade8edfc
Carlos Zamora Morales <zamorx@gmail.com>
2023-03-03 15:21:49 -0600

    PHPMailer Fixes

--- dashboard/View/notify.php
+++ dashboard/View/notify.php
@@ -19,7 +19,7 @@ $mail->isSMTP();                                            //Send using SMTP
 $mail->Host       = 'smtp.office365.com';                   //Set the SMTP server to send through
 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
 $mail->Username   = 'noreply@azedpress.com';                //SMTP username
-$mail->Password   = 'coghy0-rijnIr-binnac';                 //SMTP password
+$mail->Password   = '';                 //SMTP password
 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable implicit TLS encryption
 $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
