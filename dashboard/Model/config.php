4928e7dd21eec7a836a9638cdae665703b3101e6
Carlos Zamora Morales <zamorx@gmail.com>
2023-03-06 13:53:23 -0600

    Update Git

--- dashboard/Model/config.php
+++ dashboard/Model/config.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_HOST', 'www.azedpress.com');
+define('DB_HOST', 'localhost');
 define('DB_USER', 'azedpres_user');
 define('DB_PASSWORD', 'Azedpress@2022');
 define('DB_NAME', 'azedpres_trackingdb');
