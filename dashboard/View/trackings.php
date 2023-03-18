d3e68796f39558736881b1f2736f0289f4d0323d
Carlos Zamora Morales <zamorx@gmail.com>
2023-03-18 11:09:11 -0600

    Update trackings.php

--- dashboard/View/trackings.php
+++ dashboard/View/trackings.php
@@ -173,9 +173,10 @@
 </section>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
 <script type="text/javascript">
     $(document).ready(function() {
         $('[data-toggle="tooltip"]').popover()
-    });
+    })
 </script>
 
